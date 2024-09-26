4e788dde694b4595e9019ee746f8ea30f6f4c832
Muhammed Nehyan M <muhammednehyan9895@gmail.com>
2024-09-26 10:00:32 +0530

    different commit

--- obj/Debug/net8.0/new-app.AssemblyInfo.cs
+++ obj/Debug/net8.0/new-app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("new-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+109a927268107ff9c81d9407141ec3d640c29688")]
 [assembly: System.Reflection.AssemblyProductAttribute("new-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("new-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
